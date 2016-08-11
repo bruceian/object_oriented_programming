96000778923e101c2df6e040c245d464ad12e478
Ian Bruce <brucegordonian@gmail.com>
2016-08-11 14:39:13 -0400

    Added attributes.

--- paperboy.rb
+++ paperboy.rb
@@ -1,6 +1,14 @@
 class Paperboy
 
+  def initialize(name, experience, side, earnings)
+    @name = name
+    @experience = experience
+    @side = side
+    @earnings = earnings
+  end
 
+  def quota
+    
 
 
 end
