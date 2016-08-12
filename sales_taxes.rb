77a1df94934fa62d05f2b4d2262717c4f76ab071
Ian Bruce <brucegordonian@gmail.com>
2016-08-12 16:32:48 -0400

    Methods are working, BUT unable to output in list form.

--- sales_taxes.rb
+++ sales_taxes.rb
@@ -1,37 +1,59 @@
 class Item
 
-  def initialize(item_qty, item_name, price, item_type)
+attr_reader :item_qty, :item_name, :price, :category, :imported
+
+  def initialize(item_qty, item_name, price, category, imported)
     @item_qty = item_qty
     @item_name = item_name
-    @price = price.to_f
-    @item_type = item_type
+    @price = price
+    @category = category
+    @imported = imported
+  end
+
+  def sales_tax
+    if @category == "book"
+      @price * 0
+    elsif @category == "food"
+      @price * 0
+    elsif @category == "medical"
+      @price * 0
+    else
+      @price * 0.1
+    end
+  end
+
+  def import
+    if @imported == "imported"
+      @price * 0.05
+    else
+      @price * 0
+    end
   end
 
   def print_items
     "#{@item_qty} #{@item_name} at #{@price}"
   end
 
-  def apply_tax
-    if @item_type == "imported"
-      @price * 1.05
-    elsif @item_type == "book"
-      @price
-    elsif @item_type == "food"
-      @price
-    elsif @item_type == "medical"
-      @price
-    else
-      @price * 1.10
-    end
-  end
-
-  def print_receipt
-    "#{@item_qty} #{@item_name}: #{apply_tax}"
-  end
-
 end
 
-class Receipt
+class Receipt  Item
 
+  def initialize
+    @items = []
+  end
 
+  def add_item(item)
+    @items.push(item)
+  end
+
+ def print_receipt
+   @items.each do |item|
+     return "#{item.item_qty} #{item.item_name}: #{(item.price + item.import + item.sales_tax).round (2)}"
+      end
+ end
 end
+
+i = Item.new(1, "book", 12.49, "book", "imported")
+r = Receipt.new
+r.add_item(i)
+r.print_receipt
