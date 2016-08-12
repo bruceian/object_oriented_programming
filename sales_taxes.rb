58b23d3f5d140bf44c1979d659d86597f10c9e64
Ian Bruce <brucegordonian@gmail.com>
2016-08-12 13:05:09 -0400

    Created method within class Item.

--- sales_taxes.rb
+++ sales_taxes.rb
@@ -1,13 +1,20 @@
 class Item
 
-  def initialize
+  def initialize(item_qty, item_name, price)
+    @item_qty = item_qty
+    @item_name = item_name
+    @price = price.to_f
+  end
+
+  def item_price
+    "#{@item_qty} #{@item_name} at #{@price}"
   end
 
 end
 
-class Receipt
+class Receipt < Item
 
   def initialize
   end
 
-end   #Created two classes Item and Receipt
+end
