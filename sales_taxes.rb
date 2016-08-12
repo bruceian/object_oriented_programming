37856774ad1ddbd10a639bbef917931beafcce31
Ian Bruce <brucegordonian@gmail.com>
2016-08-12 14:34:33 -0400

    Methods work, but unable to run totals.

--- sales_taxes.rb
+++ sales_taxes.rb
@@ -1,20 +1,37 @@
 class Item
 
-  def initialize(item_qty, item_name, price)
+  def initialize(item_qty, item_name, price, item_type)
     @item_qty = item_qty
     @item_name = item_name
     @price = price.to_f
+    @item_type = item_type
   end
 
-  def item_price
+  def print_items
     "#{@item_qty} #{@item_name} at #{@price}"
+  end
+
+  def apply_tax
+    if @item_type == "imported"
+      @price * 1.05
+    elsif @item_type == "book"
+      @price
+    elsif @item_type == "food"
+      @price
+    elsif @item_type == "medical"
+      @price
+    else
+      @price * 1.10
+    end
+  end
+
+  def print_receipt
+    "#{@item_qty} #{@item_name}: #{apply_tax}"
   end
 
 end
 
-class Receipt < Item
+class Receipt
 
-  def initialize
-  end
 
 end
